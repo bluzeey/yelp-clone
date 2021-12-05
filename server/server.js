7ae473506ce76fdf884d3f9ad00f139e8409be4a
Sahil Maheshwari <sahilm1711@gmail.com>
2021-12-05 21:04:16 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ const app=express();
 app.get("/getRestaurants",(req,res)=>{
     res.send("These are the restaurants");
 })
-const port=process.env.PORT ||5001;
+const port=process.env.PORT||5001;
 app.listen(port,()=>{
     console.log('Server connected launch the fireworks!')
 })
