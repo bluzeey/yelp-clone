432bc41df0d9e98852194b8c258e9b3c2bfce6fc
Sahil Maheshwari <sahilm1711@gmail.com>
2021-12-07 09:17:15 +0530

    Adding all the routes for the application

--- server/server.js
+++ server/server.js
@@ -2,10 +2,23 @@ const express = require('express');
 
 const app=express();
 
-app.get("/getRestaurants",(req,res)=>{
-    res.send("These are the restaurants");
+
+app.get("/api/v1/restaurants",(req,res)=>{
+    res.status(200).json({
+        status:"success",
+    data:{
+        restaurant:['Mcdonalds','BurgerFarm']
+    }});
 })
-const port=process.env.PORT||5001;
-app.listen(port,()=>{
-    console.log('Server connected launch the fireworks!')
+
+app.get("api/v1/restaurants/:id",(req,res)=>(
+    console.log(req)
+))
+
+app.post("api/v1/restaurants",(req,res)=>{
+    console.log(req)
+})
+const PORT=process.env.PORT||5001;
+app.listen(PORT,()=>{
+    console.log(`Server connected launch the fireworks!${PORT}`)
 })
