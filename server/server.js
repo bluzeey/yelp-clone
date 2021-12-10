71586e6e622c60342291d02cb9a2fbd6dac545b0
Sahil Maheshwari <sahilm1711@gmail.com>
2021-12-10 09:38:21 +0530

    Added morgan

--- server/server.js
+++ server/server.js
@@ -1,8 +1,12 @@
 const express = require('express');
-
+const morgan = require('morgan');
 const app=express();
 
-
+app.use(morgan('dev'))
+app.use((req,res,next)=>{
+    console.log('Making a middleware');
+    next();
+});
 app.get("/api/v1/restaurants",(req,res)=>{
     res.status(200).json({
         status:"success",
@@ -12,11 +16,15 @@ app.get("/api/v1/restaurants",(req,res)=>{
 })
 
 app.get("api/v1/restaurants/:id",(req,res)=>(
-    console.log(req)
+    console.log(req.params)
 ))
 
 app.post("api/v1/restaurants",(req,res)=>{
     console.log(req)
+})
+
+app.put("api/v1/restaurants/:id",(req,res)=>{
+    console.log(req.body,req.params.id)
 })
 const PORT=process.env.PORT||5001;
 app.listen(PORT,()=>{
